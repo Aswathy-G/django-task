cb326770663cad8fe5327b8005e90e116d75d61b
Aswathy-G <aswathyg488@gmail.com>
2023-06-08 12:25:36 +0530

    html styling editing

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from web.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id","image","name","price","description")
+
+admin.site.register(Product, ProductAdmin)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    image = models.ImageField(upload_to="products/")
+    name =  models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
+    description = models.TextField()
+    is_delete = models.BooleanField(default = False)
+    is_edit = models.BooleanField(default = False)
+    
+
+    def __str__ (self):
+        return self.name
+   
+    
+
